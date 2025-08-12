ddd78dd523939368825904bd5c59b814beaba198
Ainsley <acairns.anna@gmail.com>
2025-08-12 15:39:55 -0500

    button update

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+module Admin
+class UsersController < ApplicationController
+    def index
+      @users = User.all
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_dashboard_path, notice: 'User was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+end
